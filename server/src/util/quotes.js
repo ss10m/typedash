ae2e3b12c2718f87b7a22fc103cef49eefa8ac3b
ss <fastf20@gmail.com>
2021-02-26 19:08:22 -0500

    added more quotes

--- server/src/util/quotes.js
+++ server/src/util/quotes.js
@@ -34,6 +34,71 @@ const quotes = [
         author: "Robert Jordan",
         source: "The Shadow Rising",
     },
+    {
+        text: `It's the questions we can't answer that teach us the most. They teach us how to think. If you give a man an answer, all he gains is a little fact. But give him a question and he'll look for his own answers.`,
+        author: "Patrick Rothfuss",
+        source: "The Wise Man's Fear",
+    },
+    {
+        text: `There are times when the mind is dealt such a blow it hides itself in insanity. While this may not seem beneficial, it is. There are times when reality is nothing but pain, and to escape that pain the mind must leave reality behind.`,
+        author: "Patrick Rothfuss",
+        source: "The Name of the Wind",
+    },
+    {
+        text: `Everyone thinks that courage is about facing death without flinching. But almost anyone can do that. Almost anyone can hold their breath and not scream for as long as it takes to die. True courage is about facing life without flinching. I don't mean the times when the right path is hard, but glorious at the end. I'm talking about enduring the boredom, the messiness, and the inconvenience of doing what is right.`,
+        author: "Robin Hobb",
+        source: "The Mad Ship",
+    },
+    {
+        text: `"It all goes back and back," Tyrion thought, "to our mothers and fathers and theirs before them. We are puppets dancing on the strings of those who came before us, and one day our own children will take up our strings and dance in our steads."`,
+        author: "George R.R. Martin",
+        source: "A Storm of Swords",
+    },
+    {
+        text: `The Wheel of Time turns, and Ages come and pass, leaving memories that become legend. Legend fades to myth, and even myth is long forgotten when the Age that gave it birth comes again. In one Age, called the Third Age by some, an Age yet to come, an Age long past, a wind rose in the Mountains of Mist. The wind was not the beginning. There are neither beginnings nor endings to the turning of the Wheel of time. But it was a beginning.`,
+        author: "Robert Jordan",
+        source: "The Eye of the World",
+    },
+    {
+        text: `Remember, if the time should come when you have to make a choice between what is right and what is easy, remember what happened to a boy who was good, and kind, and brave, because he strayed across the path of Lord Voldemort. Remember Cedric Diggory.`,
+        author: "J.K. Rowling",
+        source: "Harry Potter and the Goblet of Fire",
+    },
+    {
+        text: `There is nothing to be feared from a body, Harry, any more than there is anything to be feared from the darkness. Lord Voldemort, who of course secretly fears both, disagrees. But once again he reveals his own lack of wisdom. It is the unknown we fear when we look upon death and darkness, nothing more.`,
+        author: "J.K. Rowling",
+        source: "Harry Potter and the Half-Blood Prince",
+    },
+    {
+        text: `Voldemort himself created his worst enemy, just as tyrants everywhere do! Have you any idea how much tyrants fear the people they oppress? All of them realize that, one day, amongst their many victims, there is sure to be one who rises against them and strikes back!`,
+        author: "J.K. Rowling",
+        source: "Harry Potter and the Half-Blood Prince",
+    },
+    {
+        text: `But he understood at last what Dumbledore had been trying to tell him. It was, he thought, the difference between being dragged into the arena to face a battle to the death and walking into the arena with your head held high. Some people, perhaps, would say that there was little to choose between the two ways, but Dumbledore knew — and so do I, thought Harry, with a rush of fierce pride, and so did my parents — that there was all the difference in the world.`,
+        author: "J.K. Rowling",
+        source: "Harry Potter and the Half-Blood Prince",
+    },
+    {
+        text: `How do you go on, when in your heart you begin to understand... there is no going back? There are some things that time cannot mend. Some hurts that go too deep.`,
+        author: "J.R.R. Tolkien",
+        source: "The Return of the King",
+    },
+    {
+        text: `Vaelin knew himself to be the best swordsman among them. Dentos was master of the bow, Barkus unarmed combat, Nortah the finest rider and Caenis knew the wild like a wolf, but the sword was his.`,
+        author: "Anthony Ryan",
+        source: "Blood Song",
+    },
+    {
+        text: `Time is an unkind teacher, delivering lessons that we learn far too late for them to be useful. Years after I could have benefited from them, the insights come to me.`,
+        author: "Robin Hobb",
+        source: "Fool's Assassin",
+    },
+    {
+        text: `Sometimes it seems unfair that events so old can reach forward through the years, sinking claws into one's life and twisting all that follows it. Yet perhaps that is the ultimate justice: we are the sum of all we have done added to the sum of all that has been done to us. There is no escaping that, not for any of us.`,
+        author: "Robin Hobb",
+        source: "Fool's Fate",
+    },
 ];
 
 export default quotes;
