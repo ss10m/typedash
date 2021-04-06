330b11bd0296fb510332cf33b823bae15cd13e01
ss <fastf20@gmail.com>
2021-04-05 22:20:23 -0400

    feat(client/quotes): add quote sorting (sort by: id, play count, avg wpm, avg accuracy)

--- client/src/components/Quotes/Quotes.js
+++ client/src/components/Quotes/Quotes.js
@@ -1,9 +1,13 @@
 // Libraries & utils
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { useHistory } from "react-router-dom";
+import Select from "react-select";
 
 // Socket API
 import SocketAPI from "core/SocketClient";
+
+// Icons
+import { ImSortNumericAsc, ImSortNumbericDesc } from "react-icons/im";
 
 // Constants
 import { handleResponse, roundToFixed } from "helpers";
@@ -13,6 +17,7 @@ import "./Quotes.scss";
 
 const Quotes = () => {
     const [isCreating, setIsCreating] = useState(false);
+    const [isLoaded, setIsLoaded] = useState(false);
     const [quotes, setQuotes] = useState([]);
     const history = useHistory();
 
@@ -21,6 +26,7 @@ const Quotes = () => {
             .then(handleResponse)
             .then((data) => {
                 setQuotes(data.quotes);
+                setIsLoaded(true);
             })
             .catch(() => {});
     }, []);
@@ -42,13 +48,103 @@ const Quotes = () => {
         SocketAPI.createRoom(quoteId);
     };
 
+    if (!isLoaded) return null;
+
     return (
         <div className="quotes">
-            <div className="quotes-header">QUOTES</div>
+            <div className="quotes-header">
+                <p>QUOTES</p>
+                <Sort setQuotes={setQuotes} />
+            </div>
             <div className="list">
                 {quotes.map((quote) => (
                     <Quote key={quote.id} quote={quote} createRoom={createRoom} />
                 ))}
+            </div>
+        </div>
+    );
+};
+
+const Sort = ({ setQuotes }) => {
+    const [selected, setSelected] = useState({ value: "id", label: "QUOTE ID" });
+    const [sortOrder, setSortOrder] = useState("asc");
+
+    useEffect(() => {
+        const sortFunction = {
+            asc: (a, b) => a[selected.value] - b[selected.value],
+            desc: (a, b) => b[selected.value] - a[selected.value],
+        };
+        setQuotes((current) => [...current].sort(sortFunction[sortOrder]));
+    }, [selected, sortOrder, setQuotes]);
+
+    const options = [
+        { value: "id", label: "QUOTE ID" },
+        { value: "count", label: "PLAY COUNT" },
+        { value: "avg_wpm", label: "AVERAGE WPM" },
+        { value: "avg_acc", label: "AVERAGE ACCURACY" },
+    ];
+
+    const customStyles = useMemo(
+        () => ({
+            control: (base, state) => ({
+                ...base,
+                background: "#162029",
+                borderRadius: state.isFocused ? "3px 3px 0 0" : "3px",
+                borderColor: "#3a5068",
+                boxShadow: state.isFocused ? null : null,
+                "&:hover": {
+                    borderColor: "#56779a",
+                    cursor: "pointer",
+                },
+            }),
+            menu: (base) => ({
+                ...base,
+                marginTop: 0,
+            }),
+            menuList: (base) => ({
+                ...base,
+                padding: "10px 0",
+                background: "#2c3f54",
+                border: "1px solid #56779a",
+                borderTop: "none",
+                color: "whitesmoke",
+            }),
+            singleValue: (provided) => ({
+                ...provided,
+                color: "whitesmoke",
+            }),
+            option: (styles, state) => ({
+                ...styles,
+                cursor: "pointer",
+                background: state.isSelected ? "#3a5068" : "#2c3f54",
+                "&:hover": {
+                    background: "#56779a",
+                },
+                "&:active": {
+                    background: "#3e9dff",
+                },
+            }),
+        }),
+        []
+    );
+
+    return (
+        <div className="sorting">
+            <div className="select-graph">
+                <Select
+                    value={selected}
+                    options={options}
+                    autosize={true}
+                    styles={customStyles}
+                    onChange={setSelected}
+                    isDisabled={false}
+                />
+            </div>
+            <div
+                className="toggle"
+                onClick={() => setSortOrder((current) => (current === "asc" ? "desc" : "asc"))}
+            >
+                {sortOrder === "asc" ? <ImSortNumericAsc /> : <ImSortNumbericDesc />}
             </div>
         </div>
     );
@@ -67,7 +163,7 @@ const Quote = ({ quote, createRoom }) => {
             <div className="quote-stats">
                 <div>
                     <div className="item">
-                        <div className="header">COUNT</div>
+                        <div className="header">PLAYED</div>
                         <div className="value">{quote.count}</div>
                     </div>
 
