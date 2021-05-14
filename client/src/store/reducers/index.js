ab2097da072b08afea65bd6366bf49a6a503a21b
ss <fastf20@gmail.com>
2021-05-14 14:33:46 -0400

    refactor(client/store): removes unused redux actions/reducers

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
 import errorReducer from "./error";
 import sessionReducer from "./session";
-import roomReducer from "./room";
 
 export default combineReducers({
     error: errorReducer,
     session: sessionReducer,
-    room: roomReducer,
 });
